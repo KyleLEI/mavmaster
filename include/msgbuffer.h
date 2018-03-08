92a4f01e0b7f5df6bceee8b2827d0565b94394f7
Kyle Lei <leizhao2@gmail.com>
2018-03-08 23:07:04 +0800

    low level compilable

--- include/msgbuffer.h
+++ include/msgbuffer.h
@@ -1,18 +1,32 @@
-/*
- * msgbuffer.h
+/**
+ * @brief MAVConn message buffer class (internal)
+ * @file msgbuffer.h
+ * @author Vladimir Ermakov <vooon341@gmail.com>
  *
- *  Created on: Mar 3, 2018
- *      Author: kyle
+ * @addtogroup mavconn
+ * @{
+ */
+/*
+ * libmavconn
+ * Copyright 2014,2015,2016 Vladimir Ermakov, All rights reserved.
+ *
+ * This file is part of the mavros package and subject to the license terms
+ * in the top-level LICENSE file of the mavros repository.
+ * https://github.com/mavlink/mavros/tree/master/LICENSE.md
  */
 
-#ifndef INCLUDE_MSGBUFFER_H_
-#define INCLUDE_MSGBUFFER_H_
+#pragma once
 
-#include "mavlink/chibi/mavlink.h"
+#include <cassert>
+#include "mavlinkdef"
 
+namespace mavconn {
+/**
+ * @brief Message buffer for internal use in libmavconn
+ */
 struct MsgBuffer {
 	//! Maximum buffer size with padding for CRC bytes (280 + padding)
-	static constexpr ssize_t MAX_SIZE = MAVLINK_MAX_DIALECT_PAYLOAD_SIZE + 16;
+	static constexpr ssize_t MAX_SIZE = MAVLINK_MAX_PACKET_LEN + 16;
 	uint8_t data[MAX_SIZE];
 	ssize_t pos;
 	ssize_t len;
@@ -32,6 +46,27 @@ struct MsgBuffer {
 		// paranoic check, it must be less than MAVLINK_MAX_PACKET_LEN
 		assert(len < MAX_SIZE);
 	}
+
+	/**
+	 * @brief Buffer constructor for mavlink::Message derived object.
+	 */
+	/*
+	MsgBuffer(const mavlink::Message &obj, mavlink::mavlink_status_t *status, uint8_t sysid, uint8_t compid) :
+		pos(0)
+	{
+		mavlink::mavlink_message_t msg;
+		mavlink::MsgMap map(msg);
+
+		auto mi = obj.get_message_info();
+
+		obj.serialize(map);
+		mavlink::mavlink_finalize_message_buffer(&msg, sysid, compid, status, mi.min_length, mi.length, mi.crc_extra);
+
+		len = mavlink::mavlink_msg_to_send_buffer(data, &msg);
+		// paranoic check, it must be less than MAVLINK_MAX_PACKET_LEN
+		assert(len < MAX_SIZE);
+	}
+	*/
 
 	/**
 	 * @brief Buffer constructor for send_bytes()
@@ -58,4 +93,5 @@ struct MsgBuffer {
 		return len - pos;
 	}
 };
-#endif /* INCLUDE_MSGBUFFER_H_ */
+}	// namespace mavconn
+
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,24 @@
  *  Created on: Mar 3, 2018
  *      Author: kyle
  */
-#include "mavcomm.h"
+#include "interface.h"
+#include <unistd.h>
+#include <cstdio>
 
+void myCB(const mavlink_message_t *message, const mavconn::Framing framing){
+
+	printf("Callback executed\n");
+}
 int main(int argc, char* argv[]){
-	try{
-		MAVComm mavcomm(1,1);
-	}catch(boost::system::system_error &err){
-		printf("Error configuring serial device: %s\n",err.what());
+//	try{
+//		MAVComm mavcomm(1,1);
+//	}catch(boost::system::system_error &err){
+//		printf("Error configuring serial device: %s\n",err.what());
+//	}
+	mavconn::MAVConnInterface ser(21,78);
+	ser.message_received_cb=myCB;
+	while(true){
+		sleep(0);
 	}
-
+	//ser.ReceivedCb=NULL;
 }
