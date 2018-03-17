d5f67c065b9e9832257fef5d9581ac82dc3862cf
KyleLEI <leizhao2@gmail.com>
2018-03-17 10:38:24 +0800

    added parsing

---
+++ README.md
@@ -0,0 +1,9 @@
+# MAVMaster
+MAVLink communication for ENTERPRIZE.
+## Software Requirements
+
+ - [boost](http://www.boost.org/)
+
+## Compilation Flags
+ - -lboost_system
+ - -lpthread
--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,31 @@
 #include <cstdio>
 
 void myCB(const mavlink_message_t *message, const mavconn::Framing framing){
-
-	printf("Callback executed\n");
+	static mavlink_heartbeat_t hb;
+	static mavlink_attitude_t att;
+	switch(message->msgid){
+		case MAVLINK_MSG_ID_HEARTBEAT:
+			mavlink_msg_heartbeat_decode(message,&hb);
+			printf("Decoded Heartbeat: %d,%d,%d,%d\n",hb.type,hb.base_mode,hb.custom_mode,hb.system_status);
+			break;
+		case MAVLINK_MSG_ID_ATTITUDE:
+			mavlink_msg_attitude_decode(message,&att);
+			printf("Decoded Attitude: %d, %f, %f, %f",att.time_boot_ms,att.roll,att.pitch,att.yaw);
+			break;
+		default:
+			printf("Unknown message\n");
+	}
+	printf("TODO: publish to ROS here\n\n");
 }
+
 int main(int argc, char* argv[]){
 	mavconn::MAVConnInterface::Ptr fcu_link;
 	fcu_link=mavconn::MAVConnInterface::open_url("serial:///dev/ttyUSB0",21,78);
+
+//	fcu_link->message_received_cb=[](const mavlink_message_t *message, const mavconn::Framing framing){
+//		printf("Callback executed, long live Lambda\n");
+//		printf("TODO: publish to ROS here\n\n");
+//	};
 	fcu_link->message_received_cb=myCB;
 	while(true){
 		sleep(0);
